126449123d33bcbbac4556693a7c5adb659aabdb
dmitriy <128dmitriy128@gmail.com>
2022-01-31 17:45:56 +0500

    Full album can be played

--- music/views.py
+++ music/views.py
@@ -19,7 +19,7 @@ class HomeView(ListView):
 class AlbumDetailView(View):
     def get(self, request, ordering='AZ', *args, **kwargs):
         album = get_object_or_404(Album, id=self.kwargs['id'], slug=self.kwargs['slug'])
-        songs = get_object_or_404(Song, album=album, number_in_album=1)
+        songs = Song.objects.filter(album=album)
         template_name = 'music/album_detail.html'
         context = {'album': album, 'songs': songs}
         return render(request, template_name, context)
